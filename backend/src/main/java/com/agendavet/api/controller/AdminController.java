8fd4ebcc49fea6099746a26391706891792bfed6
felipesousac <claudioflsousa@gmail.com>
2023-07-26 21:08:44 -0300

    feat: Add validation on admin register fields

--- backend/src/main/java/com/agendavet/api/controller/AdminController.java
+++ backend/src/main/java/com/agendavet/api/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.agendavet.api.controller;
 import com.agendavet.api.domain.admin.Admin;
 import com.agendavet.api.domain.admin.AdminRegisterData;
 import com.agendavet.api.domain.admin.AdminRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,8 @@ public class AdminController {
 
     @PostMapping
     @Transactional
-    public void register(@RequestBody AdminRegisterData data) {
+    public void register(@RequestBody @Valid AdminRegisterData data) {
+
         repository.save(new Admin(data));
     }
 
--- backend/src/main/java/com/agendavet/api/domain/admin/AdminRegisterData.java
+++ backend/src/main/java/com/agendavet/api/domain/admin/AdminRegisterData.java
@@ -1,8 +1,15 @@
 package com.agendavet.api.domain.admin;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record AdminRegisterData(
+        @NotBlank
         String name,
+        @NotBlank
         String username,
+        @NotBlank
         String password,
+        @NotNull
         Boolean issuperadmin) {
 }
