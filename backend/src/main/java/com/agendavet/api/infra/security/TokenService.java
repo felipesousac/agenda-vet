acc34368f1b64b4407c4de84755293fba4bc3859
felipesousac <claudioflsousa@gmail.com>
2023-08-08 23:47:34 -0300

    feat: add adminLogin authentication

--- backend/src/main/java/com/agendavet/api/infra/security/TokenService.java
+++ backend/src/main/java/com/agendavet/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.agendavet.api.domain.user.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +27,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException ex){
             throw new RuntimeException("Erro ao gerar token jwt", ex);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("API Agenda.vet")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException ex){
+            throw new RuntimeException("Token JWT inválido ou expirado");
         }
     }
 
